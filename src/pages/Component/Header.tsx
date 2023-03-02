37742052ae6eb556c1d99d6d7fd250f33e5ae8b0
LaChancee <miharisoababef@gmail.com>
2023-03-02 22:09:55 +0100

    header amélioré

--- src/pages/Component/Header.tsx
+++ src/pages/Component/Header.tsx
@@ -1,5 +1,7 @@
 import React, {  } from 'react';
+import img from '/public/logo-celtic.png';
 import Link from 'next/link';
+import Image from 'next/image'
 const Header = () => {
     return (
         <>
@@ -22,11 +24,11 @@ const Header = () => {
                                     className="flex space-x-2 items-center"
                                 >
                                     <div aria-hidden="true" className="flex space-x-1">
-                                        <div className="h-4 w-4 rounded-full bg-gray-900 dark:bg-gray-200"></div>
+                                        <Image alt={"logo celtic-intelligence"} src={img} width={40} height={40} />
                                         <div className="h-6 w-2 bg-primary dark:bg-primaryLight"></div>
                                     </div>
                                     <span className="text-base font-bold text-gray-600 dark:text-white">
-                    INVESTITSIYI
+                    Celtic-intelligence
                   </span>
                                 </a>
                                 <label
@@ -51,7 +53,7 @@ const Header = () => {
                                                 href="#"
                                                 className="block md:px-4 hover:text-blue-600 transition hover:text-primary dark:hover:text-primaryLight"
                                             >
-                                                <span>Accueil</span>
+                                                <span>Home</span>
                                             </a>
                                         </li>
                                         <li>
@@ -59,7 +61,7 @@ const Header = () => {
                                                 href="#"
                                                 className="block md:px-4 hover:text-blue-600 transition hover:text-primary dark:hover:text-primaryLight"
                                             >
-                                                <span>Fonctionnement</span>
+                                                <span>Service</span>
                                             </a>
                                         </li>
                                         <li>
@@ -67,7 +69,15 @@ const Header = () => {
                                                 href="#"
                                                 className="block md:px-4 hover:text-blue-600 transition hover:text-primary dark:hover:text-primaryLight"
                                             >
-                                                <span>Tarif</span>
+                                                <span>Case studies</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="#"
+                                                className="block md:px-4 hover:text-blue-600 transition hover:text-primary dark:hover:text-primaryLight"
+                                            >
+                                                <span>About us</span>
                                             </a>
                                         </li>
                                         <li>
@@ -78,33 +88,11 @@ const Header = () => {
                                                 <span>Contact</span>
                                             </a>
                                         </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="block md:px-4 hover:text-blue-600 transition hover:text-primary dark:hover:text-primaryLight"
-                                            >
-                                                <span>Blog</span>
-                                            </a>
-                                        </li>
                                     </ul>
                                 </div>
 
                                 <div className="w-full space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
-                                    <a
-                                        href="#"
-                                        className="relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full focus:before:bg-primary/10 dark:focus:before:bg-primaryLight/10 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
-                                    ><Link href="/Register">
-                    <span className="relative text-sm font-semibold text-primary hover:text-blue-600 dark:text-primaryLight hover:text-blue-700">
-                      Sign Up
-                    </span>
-                                    </Link>
-                                    </a>
-                                    <a
-                                        href="#"
-                                        className="relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary dark:before:bg-primaryLight before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
-                                    >
 
-                                    </a>
                                 </div>
                             </div>
                         </div>
