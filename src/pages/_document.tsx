311f5a2126f3918f84758d75669d98b6e5f561c8
LaChancee <miharisoababef@gmail.com>
2023-03-22 09:12:16 +0100

    Création du Layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Header from "@/pages/Component/Header";
+import React from "react";
+import Footer from "@/pages/Component/Footer";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <Header/>
       <body>
         <Main />
         <NextScript />
       </body>
+        <Footer/>
     </Html>
   )
 }
