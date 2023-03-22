954d95b986615664751c19c937e79df18fd9efd8
LaChancee <miharisoababef@gmail.com>
2023-03-22 09:13:02 +0100

    Ajout de la section service & features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,12 @@ import  cercleLogo from 'public/logo-asso/le-cercle-de-la-compliance.png'
 import ACFE from '/public/logo-asso/Logo-ACFE-petit.png'
 import synfie from '/public/logo-asso/logo-synfie.png'
 import drapeaux from '/public/drapeaux-ue.jpg'
+import strategy from '/public/strategy.svg'
+import compliance from '/public/compliance.svg'
+import operationnal from '/public/operationnal.svg'
 import map from '/public/map.jpg'
 import logo from '/public/logo-celtic.png'
-import Header from "@/pages/Component/Header";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +24,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo-celtic.png" />
       </Head>
-      <Header></Header>
      <main>
          <div className="flex flex-col items-center justify-center min-h-screen ">
              <div
@@ -55,7 +57,7 @@ export default function Home() {
                                      intelligence, political risk assessments and advisory services.
                                      </p>
                                  <a href="#"
-                                    className="inline-flex items-center text-white bg-[#1A3260] hover:bg-blue-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                                    className="inline-flex items-center text-white bg-primary transition hover:bg-lightPrimary focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                                      Contact-us
                                      <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                           xmlns="http://www.w3.org/2000/svg">
@@ -107,61 +109,77 @@ export default function Home() {
          </section>
          <section className="bg-white dark:bg-gray-900">
              <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-                 <div className="max-w-screen-md mb-8 lg:mb-16">
-                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our area of expertise</h2>
+                 <div className="grid place-content-center max-w-screen-md mb-8 lg:mb-16">
+                     <h2 className="mb-4 text-4xl align-center tracking-tight font-extrabold text-gray-900 dark:text-white">Our area of expertise</h2>
                      <p className="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets
                          where technology, innovation, and capital can unlock long-term value and drive economic
                          growth.</p>
                  </div>
-                 <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                     <div>
-                         <div
-                             className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                             <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                                  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                 <path fill-rule="evenodd"
-                                       d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                                       clip-rule="evenodd"></path>
-                             </svg>
+
+                 <div className="py-16">
+                     <div className="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
+                         <div className="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
+                             <div
+                                 className="border border-gray-100 dark:border-gray-700 rounded-3xl bg-white dark:bg-gray-800 px-8 pt-12 shadow-2xl shadow-gray-600/10 dark:shadow-none sm:px-12 lg:px-8">
+                                 <div className="mb-12 space-y-4">
+                                     <div className={"flex place-content-center"}>
+                                         <Image src={strategy} alt={"illustration of man standing in front of a tab"} className={"h-1/2 w-1/2 "} width={800} height={500}/>
+                                     </div>
+                                     <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">Strategy</h3>
+                                     <ul className="mb-6 text-lg text-gray-600 dark:text-gray-300">
+                                         <li>
+                                             • Sector studies
+                                         </li>
+                                         <li>• Business Intelligence Mapping</li>
+                                     </ul>
+                                     <a href="#" className="block font-medium text-primary">Know more</a>
+                                 </div>
+                             </div>
+                             <div
+                                 className="border border-gray-100 dark:border-gray-700 rounded-3xl bg-white dark:bg-gray-800 px-8 py-12 shadow-2xl shadow-gray-600/10 dark:shadow-none sm:px-12 lg:px-8">
+                                 <div className="mb-12 space-y-4">
+                                     <div className={"flex place-content-center h-fit"}>
+                                         <Image src={compliance} alt={"illustration of man standing in front of a tab"} className={"h-1/2 w-1/2 "} width={800} height={500}/>
+                                     </div>
+                                     <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">Compliance</h3>
+                                     <ul className="mb-6 text-lg text-gray-600 dark:text-gray-300">
+                                         <li>
+                                             • Background
+                                             background checks
+                                         </li>
+                                         <li>
+                                             • Due diligence
+                                         </li>
+                                         <li>
+                                             • Support for
+                                             compliance
+                                         </li>
+                                     </ul>
+                                     <a href="#" className="block font-medium text-primary">Know more</a>
+                                 </div>
+                             </div>
+                             <div
+                                 className="border border-gray-100 dark:border-gray-700 rounded-3xl bg-white dark:bg-gray-800 px-8 py-12 shadow-2xl shadow-gray-600/10 dark:shadow-none sm:px-12 lg:px-8">
+                                 <div className="mb-12 space-y-4">
+                                     <div className={"flex place-content-center justify-self-stretch"}>
+                                         <Image src={operationnal} alt={"illustration of man standing in front of a tab"} className={"h-full w-full"} width={800} height={500}/>
+                                     </div>
+                                     <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">Operationnal</h3>
+                                     <ul className="mb-6 text-lg text-gray-600 dark:text-gray-300">
+                                         <li>
+                                             • Accompaniment anti-corruption
+                                         </li>
+                                         <li>• Export support</li>
+                                     </ul>
+                                     <a href="#" className="block font-medium text-primary">Know more</a>
+                                 </div>
+                             </div>
                          </div>
-                         <h3 className="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-                         <p className="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate
-                             seamlessly with all the organization and hit your marketing goals every month with our
-                             marketing plan.</p>
-                     </div>
-                     <div>
-                         <div
-                             className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                             <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                                  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                 <path
-                                     d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
-                             </svg>
-                         </div>
-                         <h3 className="mb-2 text-xl font-bold dark:text-white">Legal</h3>
-                         <p className="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay
-                             compliant with our structured workflows and custom permissions made for you.</p>
-                     </div>
-                     <div>
-                         <div
-                             className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                             <svg className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                                  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                 <path fill-rule="evenodd"
-                                       d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-                                       clip-rule="evenodd"></path>
-                                 <path
-                                     d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
-                             </svg>
-                         </div>
-                         <h3 className="mb-2 text-xl font-bold dark:text-white">Business Automation</h3>
-                         <p className="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and
-                             much more. Now power up with hundreds of new templates to help you get started.</p>
                      </div>
                  </div>
+
              </div>
          </section>
-
      </main>
     </>
   )
