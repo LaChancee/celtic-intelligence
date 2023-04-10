b301e0deb612b8d7c5686e40efedb2c084b432be
LaChancee <miharisoababef@gmail.com>
2023-04-10 16:15:13 +0200

    Ajustement map

--- src/pages/Component/Map.tsx
+++ src/pages/Component/Map.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
-
 const geoUrl = 'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json';
 
 export default function MapChart() {
