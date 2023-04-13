ce85fe1355f6c32d1ec52c11e89e8fc7bc9b49c2
LaChancee <miharisoababef@gmail.com>
2023-04-13 19:24:05 +0200

    Ajout de la page about

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React, {  } from 'react';
+import img from '/public/logo-celtic.png';
+import Link from 'next/link';
+import Image from 'next/image'
+const About = () => {
+    return (
+        <>
+        </>
+    )
+}
+export default About
