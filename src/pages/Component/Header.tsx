1529ab92667665e42f064beac2bf789a799ba866
LaChancee <miharisoababef@gmail.com>
2023-04-15 01:41:18 +0200

    Ajout du lien vers une nouvellle page blog

--- src/pages/Component/Header.tsx
+++ src/pages/Component/Header.tsx
@@ -72,12 +72,12 @@ const Header = () => {
                                             </a>
                                         </li>
                                         <li>
-                                            <a
-                                                href="#"
+                                            <Link
+                                                href="/about"
                                                 className="block md:px-4 hover:text-blue-600 transition hover:text-primary dark:hover:text-primaryLight"
                                             >
                                                 <span>About us</span>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <li>
                                             <a
