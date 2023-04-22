f3d1de3dfae56dfbca54f0e474da48e432fea3f7
LaChancee <miharisoababef@gmail.com>
2023-04-22 02:34:25 +0200

    Fetcher des données d'api de Strapi

---
+++ src/pages/api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    try {
+        const response = await axios.get('http://127.0.0.1:1337/api/case-studies');
+        const data = response.data;
+
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
